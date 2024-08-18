39325fdd03fcd8a67ce5bd1a4fcbc5b89abf6962
SamboHassan <sambohasan@gmail.com>
2024-08-18 21:01:39 +0100

    feat: add product model

--- ecom/api/product/views.py
+++ ecom/api/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .serializers import ProductSerializer
+from .models import Product
+# Create your views here.
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
